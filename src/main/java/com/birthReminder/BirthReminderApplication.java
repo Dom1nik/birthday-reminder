5eb5d43f70bf3779ffa3dd9b55867dcfb1a9f93d
Dom1nik <dominik.bezevan@gmail.com>
2019-08-27 20:54:33 +0200

    Initial commit

---
+++ src/main/java/com/birthReminder/BirthReminderApplication.java
@@ -0,0 +1,13 @@
+package com.birthReminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthReminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthReminderApplication.class, args);
+	}
+
+}
